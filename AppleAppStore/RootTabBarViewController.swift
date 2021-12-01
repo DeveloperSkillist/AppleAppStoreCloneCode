c333d7372821687fec041c209c600c76ebcc1edb
skillist <srpgs2@gmail.com>
2021-12-01 19:44:21 +0900

    main.스토리보드 제거 및 rootTabbarViewController 추가

---
+++ AppleAppStore/RootTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RottTabBarViewController.swift
+//  AppleAppStore
+//
+//  Created by skillist on 2021/12/01.
+//
+
+import UIKit
+
+class RootTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

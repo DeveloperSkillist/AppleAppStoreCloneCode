26c8a35d59252df8436dc183be71e4bf5e9f8a48
skillist <srpgs2@gmail.com>
2021-12-01 19:26:05 +0900

    first commit

---
+++ AppleAppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleAppStore
+//
+//  Created by skillist on 2021/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
